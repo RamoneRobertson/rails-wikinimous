f0f5b3beddff744c5ef65f0419d76a3743e2ce2a
Ramone Robertson <ramonerobertson10@gmail.com>
2024-06-30 13:43:22 +0900

    Finished displaying articles for the index

--- Gemfile
+++ Gemfile
@@ -41,6 +41,10 @@ gem "tzinfo-data", platforms: %i[ windows jruby ]
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
 
+# Manually added gems
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'main'
+gem 'forgery'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'Clearing Database....'
+Article.destroy_all
+
+
+puts 'Creating Articles...'
+10.times do
+  # Forgery(:lorem_ipsum).words(1000)
+  Article.create(
+    title: Faker::GreekPhilosophers.quote,
+    content: Faker::Lorem.paragraph(sentence_count: rand(15..40))
+  )
+end
+
+puts 'Created 10 articles'
